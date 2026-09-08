b5dfc0ff51eae841bd2e9704b66180ce84521bc6
Nikita Cherkashin <nikitacherkashi4@gmail.com>
2026-09-08 17:49:10 +0700

    Initial commit

---
+++ src/main/java/com/github/nikitacherkashi4/habituniverse/HabitUniverseApplication.java
@@ -0,0 +1,13 @@
+package com.github.nikitacherkashi4.habituniverse;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HabitUniverseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HabitUniverseApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/github/nikitacherkashi4/habituniverse/HabitUniverseApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.nikitacherkashi4.habituniverse;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HabitUniverseApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
